b5d2115183087ea305294a8991e0862be1bcf5b8
Erik Matovič <matovic.er@gmail.com>
2023-11-18 22:08:12 +0100

    Fix enemy spawning

--- Assets/Scripts/EnemySpawningScript.cs
+++ Assets/Scripts/EnemySpawningScript.cs
@@ -5,13 +5,10 @@ using UnityEngine;
 public class EnemySpawningScript : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private GameObject item;
-    [SerializeField] private float interval = 100;
-    private int _max = 1;
-    private const int StartInterval = 1, EnemyInterval = 1;
-    private bool generating = false;
+    private int _max = 2;
+    private const int StartInterval = 3, EnemyInterval = 1;
+    private bool _isGenerating = false;
     private int _generatedEnemies = 0;
-    private float _counter = 0;
     private System.DateTime _startTime;
     
     private void Start()
@@ -21,18 +18,33 @@ public class EnemySpawningScript : MonoBehaviour
 
     private void Update()
     {
-        if (_generatedEnemies >= _max) return;
+        if (_isGenerating) return;
         var ts = System.DateTime.UtcNow - _startTime;
-        if (ts.Seconds < StartInterval) return;
-        if (ts.Seconds % EnemyInterval != 0) return;
-        if (!generating)
+        if (ts.Seconds % StartInterval != 0) return;
+        StartCoroutine(Generate());
+    }
+
+    private IEnumerator Generate()
+    {
+        _isGenerating = true;
+        while(_generatedEnemies < _max)
         {
-            generating = true;
+            var ts = System.DateTime.UtcNow - _startTime;
+            if (ts.Seconds < StartInterval) continue;
+            if (ts.Seconds % EnemyInterval != 0) continue;
+            
             var transform1 = transform;
             var enemy = Instantiate(enemyPrefab, transform1.position, transform1.rotation);
             enemy.transform.SetParent(gameObject.transform);
-            generating = false;
             ++_generatedEnemies;
+            yield return StartCoroutine(Wait(1.0f));
         }
+        _isGenerating = false;
+        _generatedEnemies = 0;
+    }
+    
+    private IEnumerator Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
     }
 }
